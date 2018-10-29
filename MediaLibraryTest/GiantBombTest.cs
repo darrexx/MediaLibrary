b135df394ea6f770a3b512d59fd6d987254f5d4f
darrexx <timo.walter@udo.edu>
2018-10-29 18:54:37 +0100

    Add Test Project

---
+++ MediaLibraryTest/GiantBombTest.cs
@@ -0,0 +1,17 @@
+﻿using MediaLibrary.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MediaLibraryTest
+{
+    [TestClass]
+    public class GiantBombTest
+    {
+        [TestMethod]
+        public void TestSearch()
+        {
+            var provider = new GiantBombProvider("", "");
+            var games = provider.SearchByNameAsync("metroid prime").Result;
+            Assert.IsNotNull(games);
+        }
+    }
+}
---
+++ MediaLibraryTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MediaLibraryTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MediaLibraryTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2c4841cb-5e84-4ec9-bb65-7cc94cb9123e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
